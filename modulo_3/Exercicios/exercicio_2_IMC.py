a7eeca816543907a36e9cd3b149f9e3bd9f4269f
HadrianKalk <109358715+HadrianKalk@users.noreply.github.com>
2024-11-17 18:42:06 -0300

    Adição das aulas de operadores aritmeticos e do exercicio de IMC

---
+++ modulo_3/Exercicios/exercicio_2_IMC.py
@@ -0,0 +1,9 @@
+nome = "Mirela"
+altura = 1.65
+peso = 43
+
+imc = peso / (altura * 2)
+
+print(nome+" tem "+str(altura)/+" metros de altura")
+print("Pesa "+str(peso)+" quilos")
+print("E tem o IMC de: "+str(imc))
